5bda93c35fda50daadc4902f3f015ad5f308d0cf
Hiep Can <hiepcan@uw.edu>
2018-04-07 02:08:25 -0700

    Implement getList of animes using EJS

--- main.js
+++ main.js
@@ -5,46 +5,51 @@ const path = require('path');
 const bodyParser = require('body-parser');
 const xml2js = require('xml2js');
 const app = express();
+var fs = require('fs');
 
 var url = "https://myanimelist.net/malappinfo.php"
 var userQuery = "?u=";
 var animeQuery = "&status=all&type=anime";
 
-app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname + '/index.html'));
-})
+app.use(express.static('public'));
 
-app.post('/', function(req, res) {
-    res.sendFile(path.join(__dirname + '/index.html'));
+app.set('view engine', 'ejs');
+
+app.get('/', function (req, res) {
+    res.render('index', {animeList: null, error: null});
+});
+
+app.post('/', function (req, res) {
+    res.render('index', {animeList: null, error: null});
+});
+
+app.post('/getList', function (req, res) {
+    getList(req, res);
 })
 
 app.listen(3000, function () {
     console.log('Example app listening on port 3000!')
 })
 
-app.post('/getList', function (req, res) {
-    getList(req);
-});
-
-function getList(req) {
-    userQuery += req.body.username;
-    axios.get(url + userQuery + animeQuery, {
+function getList(req, res) {
+    var query = userQuery + req.body.username;
+    axios.get(url + query + animeQuery, {
         crossDomain: true,
     })
     .then(response => {
         xml2js.parseString(response.data, function(err, result) {
             var animes = result.myanimelist.anime;
             var animeList = [];
-            for (var anime in animes) {
-                animeList.push(anime.series_title);
+            for (var anime of animes) {
+                animeList.push(anime.series_title + '\n');
             }
+            res.render('index', {animeList: animeList, error: null});
         });
     })
     .catch(error => {
-        console.log(error);
+        res.render('index', {animeList: null, error: 'Error' + error});
     });
 };
