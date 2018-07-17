e15c9ae1c21a8d84efdc040f69fddc4bfc4bd430
Hiep Can <hiepcan@uw.edu>
2018-07-17 02:01:40 -0700

    Change project from MAL data to Kitsu data

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
-# Visual MAL
+# Visual Kitsu 
 
-Visual MAL is a website that focuses mainly on data visualization of 
-myanimelist.com profile.
+Visual Kitsu is a website that focuses mainly on data visualization of 
+kitsu.io profile.
 
 This website is inspired by [GRAPH.ANIME.PLUS](https://graph.anime.plus/).
 
@@ -15,32 +15,17 @@ Website is available at `localhost:3000`.
 - History of all anime broken down by {episode, season} by {day, week, month, year} as a timeline or a gantt chart
 - distribution of score vs {time/date, genre, rating, tags, etc}
 - Comparison of you vs friends for any of the above metric
-    - ~~Get user's friend list~~
+    - Get user's friend list
 - Recent history graph
 - Eps per day/week/month as a bar chart for >21 days ago
 
-## MyAnimeList API
+## Kitsu API
 
 **Note: Postman is recommended**
 
-[Official MAL API document](https://myanimelist.net/modules.php?go=api) 
+[Official Kitsu API document](https://myanimelist.net/modules.php?go=api) 
 
-Get user's anime list:
-```
-curl "https://myanimelist.net/malappinfo.php?status=all&type=anime&u=USERNAME"
-```
-
-**Note: The following are requests for html results and the results need to parse before using**
-
-Get user's friend list:
-```
-curl "https://myanimelist.net/profile/USERNAME/friends"
-```
-
-Get watching history per anime (may need to handle cookies/credentials to bypass login):
-```
-curl "https://myanimelist.net/ajaxtb.php?keepThis=true&detailedaid=36038&TB_iframe=true"
-```
+[Kitsu API Tutorial](https://github.com/pheyvaer/kitsu-tutorial/blob/master/index.md)
 
 ## License
 
--- main.js
+++ main.js
@@ -9,8 +9,9 @@ const cheerio = require('cheerio');
 const rp = require('request-promise');
 var fs = require('fs');
 
-var url = "https://myanimelist.net/malappinfo.php"
-var userQuery = "?u=";
+var url = "https://myanimelist.net/mlappinfo.php";
+var userQuery = "https://kitsu.io/api/edge/users?filter[name]=";
+var libraryQuery = "https://kitsu.io/api/edge/library-entries?filter[userId]=";
 var animeQuery = "&status=all&type=anime";
 
 app.use(bodyParser.json());
@@ -21,15 +22,29 @@ app.use(express.static('public'));
 app.set('view engine', 'ejs');
 
 app.get('/', function (req, res) {
-    res.render('index', {animeList: null, friendList: null, error: null});
+    res.render('index', {
+        userId: null,
+        animeList: null,
+        friendList: null,
+        error: null
+    });
 });
 
 app.post('/', function (req, res) {
-    res.render('index', {animeList: null, friendList: null, error: null});
+    res.render('index', {
+        userId: null,
+        animeList: null,
+        friendList: null,
+        error: null
+    });
+});
+
+app.post('/getUserId', function (req, res) {
+    getUserId(req, res);
 });
 
 app.post('/getList', function (req, res) {
-    getList(req, res);
+    getList(req, res).then(anime => getAnimeNames(anime, res));
 })
 
 app.post('/getFriends', (req, res) => {
@@ -40,25 +55,106 @@ app.listen(3000, function () {
     console.log('Example app listening on port 3000!')
 })
 
-function getList(req, res) {
+function getUserId(req, res) {
     var query = userQuery + req.body.username;
-    axios.get(url + query + animeQuery, {
+    axios.get(query, {
         crossDomain: true,
     })
     .then(response => {
-        xml2js.parseString(response.data, function(err, result) {
-            var animes = result.myanimelist.anime;
-            var animeList = [];
-            for (var anime of animes) {
-                animeList.push(anime.series_title + " ");
-            }
-            res.render('index', {animeList: animeList, friendList: null, error: null});
+        var data = response.data.data;
+        var id = data[0].id;
+        res.render('index', {
+            userId: id,
+            animeList: null,
+            friendList: null,
+            error: null
         });
     })
     .catch(error => {
-        res.render('index', {animeList: null, friendList: null, error: 'Error' + error});
-    });
+        res.render('index', {
+            userId: null,
+            animeList: null,
+            friendList: null,
+            error: 'Error: ' + error
+        });
+    })
+}
+
+async function getList(req, res) {
+    var query = libraryQuery + req.body.userId;
+    var totalAnime = 0;
+    var anime = [];
+    async function libraryRequest() {
+        await axios.get(query, {
+            crossDomain: true,
+        })
+        .then(async response => {
+            var data = response.data.data;
+            totalAnime = response.data.meta.count;
+            var animeData = Array.from(data, x => x.relationships.anime.links.related);
+            anime = anime.concat(animeData);
+            query = response.data.links.next;
+            if (query != undefined) {
+                await libraryRequest();
+            }
+        })
+        .catch(error => {
+            res.render('index', {
+                userId: null,
+                animeList: null,
+                friendList: null,
+                error: 'Error: ' + error
+            });
+        });
+    };
+
+    await libraryRequest();
+
+    return anime;
 };
+
+async function getAnimeNames(anime, res) {
+    var titleList = [];
+
+    await (async () => {
+        for (const query of anime) {
+            await axios.get(query, {
+                crossDomain: true,
+            })
+            .then(response => {
+                var data = response.data.data;
+                if (data != null) {
+                    var attr = data.attributes;
+                    var titles = attr.titles;
+                    if (titles.en_jp != undefined) {
+                        titleList.push(titles.en_jp);
+                    } else if (titles.en_cn != undefined) {
+                        titleList.push(titles.en_cn);
+                    } else if (titles.en_us != undefined) {
+                        titleList.push(titles.en_us);
+                    } else if (titles.en != undefined) {
+                        titleList.push(titles.en);
+                    }
+                }
+            })
+            .catch(error => {
+                res.render('index', {
+                    userId: null,
+                    animeList: null,
+                    friendList: null,
+                    error: 'Error: ' + error
+                });
+            });
+        };
+    })();
+
+    res.render('index', {
+        userId: null,
+        animeList: titleList,
+        friendList: null,
+        error: null
+    });
+}
 
 function getFriends(req, res) {
     var username = req.body.username;
@@ -76,9 +172,19 @@ function getFriends(req, res) {
                 var friend = data(elem).find('div > a > strong').html()
                 friendList.push(friend)
             })
-            res.render('index', {animeList: null, friendList: friendList, error: null});
+            res.render('index', {
+                userId: null,
+                animeList: null,
+                friendList: friendList,
+                error: null
+            });
         })
         .catch((err) => {
-            res.render('index', {animeList: null, friendList: null, error: 'Error' + error});
+            res.render('index', {
+                userId: null,
+                animeList: null,
+                friendList: null,
+                error: 'Error' + error
+            });
         });
 };
